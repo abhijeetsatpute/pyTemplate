808a0c9a15d9fe12a6f7d25749b5d3ff92da6bfc
Abhijeet Satpute <abhi111sat@gmail.com>
2021-01-14 11:00:52 +0530

    AUTHOR variable

--- create.py
+++ create.py
@@ -1,6 +1,7 @@
 import sys
 import time
+AUTHOR = 'abhi'
 
 f= open(sys.argv[1]+'.py',"w+")
-f.write("'''\nCreated on \n" + time.ctime(int(time.time())) + "\n@author:abhi\n'''\n")
+f.write("'''\nCreated on \n" + time.ctime(int(time.time())) + "\n@author: " + AUTHOR + "\n'''\n")
 f.close()
